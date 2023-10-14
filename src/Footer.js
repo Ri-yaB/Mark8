86edc3168b09e901a6bded181fa0a635ff337194
Riya bansal <103059954+Ri-yaB@users.noreply.github.com>
2023-10-14 15:18:28 +0530

    sections

---
+++ src/Footer.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import './styles.css';
+import image from "./images/Mark.png"
+import { FaFacebook, FaTwitter, FaGooglePlusG } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className="footer-section">
+      <div className="container">
+        {/* Second Row */}
+        <div className="footer-content">
+          <div className="footer-widget col">
+            <div className="footer-logo">
+              <a href="index.html"><img src={image} alt="logo" /></a>
+            </div>
+            <div className="footer-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntut consectetur adipisicing elit, Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="footer-social-icon">
+              <span>Follow us</span>
+              <a href="#">
+            <FaFacebook className="facebook-bg" />
+          </a>
+          <a href="#">
+            <FaTwitter className="twitter-bg" />
+          </a>
+          <a href="#">
+            <FaGooglePlusG className="google-bg" />
+          </a>
+            </div>
+          </div>
+          <div className="footer-widget col">
+            <div className="footer-widget-heading">
+              <h3>Contact Us</h3>
+            </div>
+            <div className="footer-text mb-25">
+              <p>If you have any questions or inquiries, please feel free to contact us.</p>
+            </div>
+            <div className="contact-info">
+              <p><i className="fas fa-map-marker-alt"></i> something</p>
+              <p><i className="fas fa-phone"></i> 2739498499290</p>
+              <p><i className="far fa-envelope-open"></i> mail@info.com</p>
+            </div>
+          </div>
+        {/*  <div className="footer-widget col">
+            <div className="footer-widget-heading">
+              <h3>Useful Links</h3>
+            </div>
+            <ul>
+              <li><a href="#">Home</a></li>
+              
+              
+              
+            </ul>
+  </div>*/}
+          <div className="footer-widget col">
+            <div className="footer-widget-heading">
+              <h3>Subscribe</h3>
+            </div>
+            <div className="footer-text mb-25">
+              <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+            </div>
+            <div className="subscribe-form">
+              <form action="#">
+                <input type="text" placeholder="Email Address" />
+                <button><i className="fab fa-telegram-plane"></i></button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        {/* Third Row 
+        <div className="copyright-area">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>Copyright &copy; 2018, All Right Reserved <a href="https://mymark8.com/">Mark8</a></p>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Terms</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Policy</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>*/}
+
+<div className="copyright-area">
+          <div className="row">
+            <div className="col-6 text-left">
+              <div className="copyright-text">
+                <p>Copyright &copy; 2018, All Rights Reserved <a href="https://mymark8.com/">Mark8</a></p>
+              </div>
+            </div>
+            <div className="col-6 text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li><a href="#">Terms</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Policy</a></li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/Hero.js
+++ src/Hero.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faHandshake } from '@fortawesome/free-solid-svg-icons'; // Import the user and handshake icons
-import "./styles.css";
+import Slider from 'react-slick'; // Import the slider library
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './styles.css';
+import image from "./images/Mark.png";
+
+
 function Hero() {
-    return (
-      <div className="banner">
-        <div className="banner-content">
-          <h1>Unlocking Influencer <br/>Commerce for Creators</h1>
-          <p>Where Creators and brands join forces to sell and earn</p>
-          
-          <button>Get Started</button>
-        </div>
-  
-        <div className="section">
+
+    const cardData = [
+        {
+          title: 'Create a profile for business enquiries',
+          content: 'Just like building a profile on LinkedIn, creators can add relevant details about their fees, availability, location, genre, etc.',
+        },
+        {
+          title: 'Connect with brands for influencer marketing',
+          content: 'Brands can search and discover the perfect creators in their industry for product promotions and directly chat with them.',
+        },
+        {
+          title: 'Monetize your audience',
+          content: 'Creators can make videos sponsoring a product for a brand and earn an 8% share of revenue when a viewer buys the product in the app.',
+        },
+        
+      ];
+
+
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h1>Unlocking Influencer <br/>Commerce for Creators</h1>
+        <p>Where Creators and brands join forces to sell and earn</p>
+        <button>Get Started</button>
+      </div>
+
+      <div className="section">
         <div className="column">
           <div className="icon">
             <FontAwesomeIcon icon={faUser} size="3x" />
           </div>
           <div className="content">
             <h2>Create your professional creator profile for business</h2>
-            
           </div>
         </div>
         <div className="column">
@@ -28,12 +50,28 @@ function Hero() {
           </div>
           <div className="content">
             <h2>Get in touch with over 10k+ brands for sponsorships</h2>
-            
           </div>
         </div>
       </div>
+      
 
+
+
+      {/* Card Grid Section */}
+<div className="section">
+        <div className="card-grid">
+          {cardData.map((card, index) => (
+            <div className="card" key={index}>
+              <h3>{card.title}</h3>
+              <p>{card.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    );
-  }
-  export default Hero
+
+
+    </div>
+  );
+}
+
+export default Hero;
